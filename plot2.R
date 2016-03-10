1e155756b7c6dad8ad895ee885c9cd3f092d0710
aimod62 <astrid.melhado@gmail.com>
2016-03-10 01:51:49 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,14 @@
+
+
+source("Loading the Data.R")
+#plot.2
+plot(time_set, set$Global_active_power, 
+     xlab = "",
+     ylab = "Global Active Power(kilowatts)",
+     type = "l")
+
+#Saving the file
+dev.copy(png,  file = "plot2.png", height = 480, width = 480)
+dev.off()
+
+#text in x axis is attributed to computer default settings in Spanish.
