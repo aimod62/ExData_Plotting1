0c6b5a91d3bd775350ae0e84658a9b4a83161231
aimod62 <astrid.melhado@gmail.com>
2016-03-10 09:43:03 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -11,4 +11,3 @@ legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
 dev.copy(png,  file = "plot3.png", height = 480, width = 480)
 dev.off()         
 
-#text in x axis is attributed to default computer settings in Spanish         
