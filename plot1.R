1196ef11639fdb54ac2ed12fe2c884bb9aa62640
aimod62 <astrid.melhado@gmail.com>
2016-03-10 01:43:52 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,8 @@
+source("Loading the Data.R") 
+#plot.1
+hist(set$Global_active_power, main = "Global Active Power",
+     xlab = "Global Active Power(kilowatts)", col = "red")
+
+#Saving the file
+dev.copy(png,  file = "plot1.png", height = 480, width = 480)
+dev.off()
